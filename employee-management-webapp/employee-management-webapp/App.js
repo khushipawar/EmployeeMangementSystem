3d398bd31c2fe5d21f81488b9302ad7bbfbd5d0f
Khushi Pawar <88932835+khushipawar@users.noreply.github.com>
2023-08-09 17:00:40 +0530

    Update App.js

--- employee-management-webapp/employee-management-webapp/App.js
+++ employee-management-webapp/employee-management-webapp/App.js
@@ -1,100 +1,75 @@
+import React from "react";
+import {
+    Table, Icon, Popconfirm, Menu, Dropdown, Button, Modal, Input, notification
+} from "antd";
+import {DndProvider} from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend";
+import {EditableColumnProps} from "antd-editable-component/cell";
+import styled from "styled-components";
+import {connect} from "react-redux";
+import {isEqual} from "lodash";
+import {DraggableBodyRow, EditableCell,} from "./DraggableTable";
+import {DelimitedFileColumn, FixedLengthFileSegment} from "../../../types";
+import MapColumn from "../../../types/map/children/MapColumn";
+import FileType from "../../../types/enums/FileType";
+import TargetDetails from "../../../types/map/children/TargetDetails";
+import FileColumnProperty from "../../../types/map/children/FileColumnProperty";
+import {Link} from "react-router-dom";
+import {sign} from "crypto";
+const { confirm } = Modal;
 
-import React, { useState } from 'react';
-import { Select, Button, Table, Input, Space } from 'antd';
-import { DeleteOutlined } from '@ant-design/icons';
+class TypedTable extends Table<MapDetailsRow> {
+}
 
-const { Option } = Select;
+const StyledTable = styled(TypedTable)`
+   .ant-table-tbody{
+    background-color: white;
+  }
+  .ant-table-thead{
+    position:sticky;
+    top:0;
+    z-index:9;
+  }
+  tr.drop-over-downward td {
+    border-bottom: 2px dashed #1890ff;
+  }
 
-const ViewMatcherListContainer = () => {
-  const [mode, setMode] = useState('view');
-  const [data, setData] = useState([
-    { key: 1, FileName: 'File1.txt', Notes: 'Note 1' },
-    { key: 2, FileName: 'File2.txt', Notes: 'Note 2' },
-    { key: 3, FileName: 'File3.txt', Notes: 'Note 3' },
-  ]);
+  tr.drop-over-upward td {
+    border-top: 2px dashed #1890ff;
+  }
+`;
 
-  const handleModeChange = value => {
-    setMode(value);
-  };
+const ViewMatcherDetailsTable = ({mapdataSource}) =>
+{
+ const columns = [
+{
+  title: 'Row',
+  dataIndex: 'rowNumber',
+  key: 'rowNumber',
+  render: (text, record, index) => index + 1,
+},
+ {
+ title: 'Matchers',
+ dataIndex: 'matcher',
+ key: 'matcher',
 
-  const handleInputChange = (e, key, dataIndex) => {
-    const updatedData = data.map(row =>
-      row.key === key ? { ...row, [dataIndex]: e.target.value } : row
-    );
-    setData(updatedData);
-  };
+ },
+ {
+ title: 'Notes',
+ dataIndex: 'notes',
+ key: 'notes',
+ },
 
-  const handleDeleteRow = key => {
-    const updatedData = data.filter(row => row.key !== key);
-    setData(updatedData);
-  };
+ ];
 
-  const columns = [
-    {
-      title: 'Row',
-      dataIndex: 'key',
-      key: 'key',
-    },
-    {
-      title: 'FileName',
-      dataIndex: 'FileName',
-      key: 'FileName',
-      render: (text, record) =>
-        mode === 'edit' ? (
-          <Input
-            value={text}
-            onChange={e => handleInputChange(e, record.key, 'FileName')}
-          />
-        ) : (
-          text
-        ),
-    },
-    {
-      title: 'Notes',
-      dataIndex: 'Notes',
-      key: 'Notes',
-      render: (text, record) =>
-        mode === 'edit' ? (
-          <Input
-            value={text}
-            onChange={e => handleInputChange(e, record.key, 'Notes')}
-          />
-        ) : (
-          text
-        ),
-    },
-    {
-      title: 'Action',
-      dataIndex: 'action',
-      key: 'action',
-      render: (_, record) =>
-        mode === 'edit' ? (
-          <Space size="middle">
-            <Button type="link" icon={<DeleteOutlined />} onClick={() => handleDeleteRow(record.key)} />
-          </Space>
-        ) : null,
-    },
-  ];
+ const fileNameMatchers = mapdataSource?.fileNameMatchers || [];
 
-  return (
-    <div>
-      <Select value={mode} onChange={handleModeChange} style={{ width: 120 }}>
-        <Option value="view">View</Option>
-        <Option value="edit">Edit</Option>
-      </Select>
 
-      {mode === 'edit' && (
-        <div>
-          <Button type="primary">Cancel</Button>
-          <Button type="primary">Add</Button>
-          <Button type="primary">Upload</Button>
-          <Button type="primary">Save</Button>
-        </div>
-      )}
 
-      <Table dataSource={data} columns={columns} />
-    </div>
-  );
+ return (
+
+ <StyledTable dataSource={fileNameMatchers} columns={columns} pagination={true} />
+ );
 };
 
-export default ViewMatcherListContainer;
+export default ViewMatcherDetailsTable;
