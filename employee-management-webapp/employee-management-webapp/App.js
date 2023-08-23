c4108f1876fe92c361c09fca2f016cb611d4a4bd
Khushi Pawar <88932835+khushipawar@users.noreply.github.com>
2023-08-24 01:19:23 +0530

    Update App.js

--- employee-management-webapp/employee-management-webapp/App.js
+++ employee-management-webapp/employee-management-webapp/App.js
@@ -1,76 +1,30 @@
-import React, { useState } from "react";
-import { Button, Popconfirm } from "antd";
-import { RouterProps } from "react-router";
-import { RightAlignContainer } from "../../shared/Styled";
-import { FileMap, MapStatus } from "../../../types";
-
-interface ViewButtonProps extends RouterProps{
-  map: FileMap;
-  popconfirmVisible: boolean;
-  setPopconfirmVisible: Function;
-  popconfirmLoading: boolean;
-  setPopconfirmLoading: Function;
-  popConfirmWarningMessage: Function;
-  saveMap: Function;
-  closeWarning: Function;
-  getUnansweredAttributes: Function;
+function modeViewOrEdit(map: FileMap | null ,value: any, setMode: (value: (((prevState: string) => string) | string)) => void,  setMap: (value: (((prevState: (FileMap | null)) => (FileMap | null)) | FileMap | null)) => void, setCopyName: (value: (((prevState: string) => string) | string)) => void, setSpecialId) {
+  if (value === "view") {
+    setMode(value);
+  }
+  if(value==="edit")
+  {
+    fetchMapByMode(map.fileMapID,value).then((res) => {
+      res.originalMap.fileNameMatchers=[];
+      mapFetch(res, setMap, setCopyName, setSpecialId);
+    });
+    setMode(value);
+  }
 }
 
-/**
- * Save and cancel buttons on the view details page (single map)
- * @param props see above, controls the types of buttons and pop confirm
- * @constructor
- */
-const ViewButtons: React.FC<ViewButtonProps> = (props: ViewButtonProps) => {
-    const [saveLoading, setSaveLoading] = useState<boolean>(false);
-    const [uploadLoading, setUploadLoading] = useState<boolean>(false);
-    const [addLoading, setAddLoading] = useState<boolean>(false);
-    
-  const {
-    map, popconfirmVisible, setPopconfirmVisible, popconfirmLoading, setPopconfirmLoading, popConfirmWarningMessage, saveMap, closeWarning, getUnansweredAttributes, history,
-  } = props;
-  return (
-    <RightAlignContainer>
-      <Popconfirm
-        placement="topRight"
-        title={popConfirmWarningMessage()}
-        visible={popconfirmVisible}
-        okText="Continue"
-        okButtonProps={{
-          loading: popconfirmLoading,
-        }}
-        onConfirm={() => {
-          setPopconfirmLoading(true);
-          saveMap(map, () => setSaveLoading(false));
-          setPopconfirmVisible(false);
-        }}
-        onCancel={() => closeWarning(() => setSaveLoading(false))}
-      >
-      <Button
-               type="primary"
-               style={{ float: "right", marginTop: "2px", marginLeft: "10px" }}
-               >
-                Save
-              </Button>
 
-       <Button
-               type="primary"
-               style={{ float: "right", marginTop: "2px", marginLeft: "10px" }}
-               >
-                Upload
-              </Button>
 
-        <Button
-               type="primary"
-                style={{ float: "right", marginTop: "2px", marginLeft: "10px" }}
-                
-                >
-                Add
-                </Button>
-      </Popconfirm>
-      <Button onClick={() => history.push("/matcherView")} style={{ float: "right", marginTop: "2px" }}>Cancel</Button>
-    </RightAlignContainer>
-  );
-};
-
-export default ViewButtons;
+export const fetchMapByMode = (mapId: number, mode:any) => instance
+  .get<FileMap>(`/fileMap/${mapId}`, { params: { navigateType: mode } })
+  .then((res) => {
+    if (res.status === 200) {
+      const originalMap = res.data;
+      const formattedMap = formatMap(res.data);
+      return { originalMap, formattedMap };
+    }
+    throw new Error("Error getting map from server.");
+  })
+  .catch((error) => {
+    history.push("/view");
+    handleError(error);
+  });
