e8b161dc1212ebc6bb2793016705c75715b79b1d
Khushi Pawar <88932835+khushipawar@users.noreply.github.com>
2023-09-08 14:21:16 +0530

    Update App.js

--- employee-management-webapp/employee-management-webapp/App.js
+++ employee-management-webapp/employee-management-webapp/App.js
@@ -1,1 +1,19 @@
+  if (dateFormat) {
 
+            return (
+                <Input
+                    type="text"
+                    name={key===3?`transformations[uniqueID].elseValue` : (key===2)?`logicalDerivationRowsets.[${index}].thenValue`:`valueTransformation.[${index}].mappedValue`}
+                    style={{width: 180, marginLeft: 10}}
+                    placeholder={(key===3)?"Else":key ===2?"Then":"Map to"}
+                    onKeyUp={(e: any) => this.handleKey(e, index, uniqueID)}
+                    onChange={(e: any) => key===3? this.handleChange(e, index, uniqueID, "elseInd") : key===2?this.handleChange(e, index, uniqueID, "thenValue"):this.handleChange(e, index, uniqueID, "mappedValue")}
+                    value={key=== 3 ? this.getElse(item.elseValue, uniqueID) : key===2? item.thenValue:item.mappedValue}
+                    allowClear
+                    disabled={(key === 3 ? this.getDisabled(item.elseInd, item.blankInd, hardcoded, "elseInd")
+                                                    :key===2 ? this.getDisabled(item.thenValue, item.blankInd, hardcoded, "thenValue")
+                                                    :this.getDisabled(item.mappedValue, item.blank, hardcoded, "mappedValue")) || !editing}
+
+                />
+            );
+        }
