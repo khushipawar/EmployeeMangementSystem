ecc4ce28cb04ee2dbf325c64ed3d6e43ba2cfea2
Khushi Pawar <88932835+khushipawar@users.noreply.github.com>
2023-09-15 19:30:22 +0530

    Update App.js

--- employee-management-webapp/employee-management-webapp/App.js
+++ employee-management-webapp/employee-management-webapp/App.js
@@ -1,4 +1,94 @@
- componentDidUpdate(prevProps: EditableCellProps) {                                                                                                                                                 
+handleDateChange = (e, index, uniqueID, key,conIndex) => {
+        const transformations = {...this.state.transformations}
+          if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 &&
+               e.keyCode <= 105)) {
+               let size = e.target.value.length;
+               if ((size == 2 && e.target.value < 13) || (size == 5 && Number(e.target.value.split('-')[1]) < 32)) {
+                   e.target.value += '-';
+           }
+        if (key == "mappedValue") {
+
+            transformations[uniqueID].valueTransformation[index].mappedValue = e.target.value;
+        }
+        if (key == "thenValue") {
+
+                    transformations[uniqueID].logicalDerivationRowsets[index].thenValue = e.target.value;
+        }
+
+        if (key == "elseInd") {
+            transformations[uniqueID].elseValue = e.target.value;
+        }
+
+        if (key == "comparisonValue") {
+        let targetId
+        this.props.dataSource.forEach(data=>{
+        data.fileColumnProperties.forEach(fileProps=>{
+        if(fileProps.fileColumnTargetValueID=== transformations[uniqueID].logicalTransformation[index].comparisonFileColumnTargetValueID){
+        targetId = fileProps.targetValueID
+        }
+        })
+        })
+            if (targetId != null) {
+                fetchTargetValidations(targetId)
+                    .then((targetDetail) => {
+                        if (targetDetail) {
+                            const enumOptions = this.parseEnumOptions(targetDetail);
+                            const isBoolean = targetDetail.validations.some((validation) => validation.errorType === ValidationErrorType.BOOLEAN);
+                            const isDate = targetDetail.validations.some((validation) => validation.errorType === ValidationErrorType.DATE);
+                            this.setState({
+                                logicalEnumOptions: enumOptions,
+                                logicalBoolean: isBoolean,
+                                logicalDate : isDate,
+                                logicalIndex: index
+                            });
+                        }
+                    });
+            }
+            transformations[uniqueID].logicalTransformation[index].comparisonValue = e.target.value;
+        }
+           if (key == "comparisonValueDer") {
+                let targetId
+                   this.props.dataSource.forEach(data=>{
+                   data.fileColumnProperties.forEach(fileProps=>{
+                   if(fileProps.fileColumnTargetValueID=== transformations[uniqueID].logicalDerivationRowsets[index].logicalDerivationConditions[conIndex].comparisonFileColumnTargetValueID){
+                   targetId = fileProps.targetValueID
+                   }
+                   })
+                   })
+                    if (targetId != null) {
+                        fetchTargetValidations(targetId)
+                            .then((targetDetail) => {
+                                if (targetDetail) {
+                                    const enumOptions = this.parseEnumOptions(targetDetail);
+                                    const isBoolean = targetDetail.validations.some((validation) => validation.errorType === ValidationErrorType.BOOLEAN);
+                                    const isDate = targetDetail.validations.some((validation) => validation.errorType === ValidationErrorType.DATE);
+                                    this.setState({
+                                        logicalDerEnumOptions: enumOptions,
+                                        logicalDerBoolean: isBoolean,
+                                        logicalDerDate: isDate,
+                                        logicalDerIndex: conIndex
+                                    });
+                                }
+                            });
+                    }
+
+                    transformations[uniqueID].logicalDerivationRowsets[index].logicalDerivationConditions[conIndex].comparisonValue = e.target.value;
+
+                }
+        this.setState({transformations: transformations})
+        }
+        this.setState({dateError: true})
+
+    }
+
+
+
+
+
+
+
+
+componentDidUpdate(prevProps: EditableCellProps) {                                                                                                                                                 
                                                                                                                                                                                                     
      const {dataSource, selectedTargets, index} = this.props;                                                                                                                                       
                          console.log("Hi I am Inside! Assign : "+ setAssigned);                                                                                                                     
