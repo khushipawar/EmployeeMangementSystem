823e2856e8889f77b871286013a06df2d2b19191
Khushi Pawar <88932835+khushipawar@users.noreply.github.com>
2023-09-17 20:04:40 +0530

    Update App.js

--- employee-management-webapp/employee-management-webapp/App.js
+++ employee-management-webapp/employee-management-webapp/App.js
@@ -1,117 +1,59 @@
-handleDateChange = (e, index, uniqueID, key,conIndex) => {
-        const transformations = {...this.state.transformations}
-          if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 &&
-               e.keyCode <= 105)) {
-               let size = e.target.value.length;
-               if ((size == 2 && e.target.value < 13) || (size == 5 && Number(e.target.value.split('-')[1]) < 32)) {
-                   e.target.value += '-';
-           }
-        if (key == "mappedValue") {
+componentDidUpdate(prevProps) {
+  const { dataSource, index, mode, datalndex } = this.props;
+  const prevMode = prevProps.mode;
 
-            transformations[uniqueID].valueTransformation[index].mappedValue = e.target.value;
-        }
-        if (key == "thenValue") {
+  if (mode === "edit" && mode !== prevMode) {
+    // Recheck conditions as in componentDidMount
+    const hardcodedData =
+      dataSource &&
+      dataSource[index]?.fileColumnProperties
+        .filter((property) => property.hardcodedValue !== null)
+        .map((property) => property.hardcodedValue).length > 0;
 
-                    transformations[uniqueID].logicalDerivationRowsets[index].thenValue = e.target.value;
-        }
+    const logicalTransformsData =
+      dataSource &&
+      dataSource[index]?.fileColumnProperties.filter(
+        (property) => property.logicalTransforms
+      ).length > 0;
 
-        if (key == "elseInd") {
-            transformations[uniqueID].elseValue = e.target.value;
-        }
+    const logicalTransformationLength = logicalTransformsData
+      ? dataSource[index]?.fileColumnProperties.filter(
+          (property) => property.logicalTransforms
+        ).length
+      : 0;
 
-        if (key == "comparisonValue") {
-        let targetId
-        this.props.dataSource.forEach(data=>{
-        data.fileColumnProperties.forEach(fileProps=>{
-        if(fileProps.fileColumnTargetValueID=== transformations[uniqueID].logicalTransformation[index].comparisonFileColumnTargetValueID){
-        targetId = fileProps.targetValueID
-        }
-        })
-        })
-            if (targetId != null) {
-                fetchTargetValidations(targetId)
-                    .then((targetDetail) => {
-                        if (targetDetail) {
-                            const enumOptions = this.parseEnumOptions(targetDetail);
-                            const isBoolean = targetDetail.validations.some((validation) => validation.errorType === ValidationErrorType.BOOLEAN);
-                            const isDate = targetDetail.validations.some((validation) => validation.errorType === ValidationErrorType.DATE);
-                            this.setState({
-                                logicalEnumOptions: enumOptions,
-                                logicalBoolean: isBoolean,
-                                logicalDate : isDate,
-                                logicalIndex: index
-                            });
-                        }
-                    });
-            }
-            transformations[uniqueID].logicalTransformation[index].comparisonValue = e.target.value;
-        }
-           if (key == "comparisonValueDer") {
-                let targetId
-                   this.props.dataSource.forEach(data=>{
-                   data.fileColumnProperties.forEach(fileProps=>{
-                   if(fileProps.fileColumnTargetValueID=== transformations[uniqueID].logicalDerivationRowsets[index].logicalDerivationConditions[conIndex].comparisonFileColumnTargetValueID){
-                   targetId = fileProps.targetValueID
-                   }
-                   })
-                   })
-                    if (targetId != null) {
-                        fetchTargetValidations(targetId)
-                            .then((targetDetail) => {
-                                if (targetDetail) {
-                                    const enumOptions = this.parseEnumOptions(targetDetail);
-                                    const isBoolean = targetDetail.validations.some((validation) => validation.errorType === ValidationErrorType.BOOLEAN);
-                                    const isDate = targetDetail.validations.some((validation) => validation.errorType === ValidationErrorType.DATE);
-                                    this.setState({
-                                        logicalDerEnumOptions: enumOptions,
-                                        logicalDerBoolean: isBoolean,
-                                        logicalDerDate: isDate,
-                                        logicalDerIndex: conIndex
-                                    });
-                                }
-                            });
-                    }
+    const logicalDerivationData =
+      dataSource &&
+      dataSource[index]?.fileColumnProperties.filter(
+        (property) => property.logicalDerivationRowsets
+      ).length > 0;
 
-                    transformations[uniqueID].logicalDerivationRowsets[index].logicalDerivationConditions[conIndex].comparisonValue = e.target.value;
+    const logicalDerivationLength = logicalDerivationData
+      ? dataSource[index]?.fileColumnProperties.filter(
+          (property) => property.logicalDerivationRowsets
+        ).length
+      : 0;
 
-                }
-        this.setState({transformations: transformations})
-        }
-        this.setState({dateError: true})
-
+    if (
+      dataSource &&
+      dataSource[index]?.fileColumnProperties
+        .flatMap((property) => property.columnTransforms).length > 0 ||
+      (dataSource[index]?.fileColumnProperties.filter(
+        (property) => property.logicalTransforms
+      ).length > 0 &&
+        logicalTransformationLength > 0) ||
+      (dataSource[index]?.fileColumnProperties.filter(
+        (property) => property.logicalDerivationRowsets
+      ).length > 0 &&
+        logicalDerivationLength > 0) ||
+      (dataSource[index]?.fileColumnProperties
+        .filter((property) => property.hardcodedValue !== null)
+        .map((property) => property.hardcodedValue).length > 0 &&
+        hardcodedData > 0)
+    ) {
+      this.setAssigned(true);
+    } else {
+      this.setAssigned(false);
     }
-
-
-
-
-
-
-
-
-componentDidUpdate(prevProps: EditableCellProps) {                                                                                                                                                 
-                                                                                                                                                                                                    
-     const {dataSource, selectedTargets, index} = this.props;                                                                                                                                       
-                         console.log("Hi I am Inside! Assign : "+ setAssigned);                                                                                                                     
-     if (dataSource && (prevProps.dataSource[index]?.fileColumnProperties?.length !== dataSource[index]?.fileColumnProperties?.length)) {                                                           
-                                                                                                                                                                                                    
-         if (dataSource[index]?.fileColumnProperties.flatMap((property) => property.columnTransforms).length === 0 &&                                                                               
-             dataSource[index]?.fileColumnProperties.flatMap((property) => property.logicalTransforms).length === 0  &&                                                                             
-             dataSource[index]?.fileColumnProperties.flatMap((property) => property.logicalDerivationRowsets).length === 0  &&                                                                      
-             dataSource[index]?.fileColumnProperties.flatMap((property) => property.elseValue === null)&&                                                                                           
-             dataSource[index]?.fileColumnProperties.flatMap((property) => property.blankInd === true) &&                                                                                           
-             dataSource[index].fileColumnProperties.filter((property) => property.hardcodedValue === null).map((property) => property.hardcodedValue).length === 0) {                               
-             this.setAssigned(false);                                                                                                                                                               
-                                                                                                                                                                                                    
-         }                                                                                                                                                                                          
-                                                                                                                                                                                                    
-         if (dataSource &&(dataSource[index]?.fileColumnProperties.flatMap((property) => property.columnTransforms).length > 0 &&                                                                   
-             dataSource[index]?.fileColumnProperties.filter((property) => property.logicalTransforms).length > 0 &&                                                                                 
-             dataSource[index]?.fileColumnProperties.flatMap((property) => property.logicalDerivationRowsets).length> 0  &&                                                                         
-             dataSource[index]?.fileColumnProperties.flatMap((property) => property.elseValue !== null) &&                                                                                          
-             dataSource[index]?.fileColumnProperties.flatMap((property) => property.blankInd !== false) &&                                                                                          
-             dataSource[index]?.fileColumnProperties.filter((property) => property.hardcodedValue !== null).map((property) => property.hardcodedValue).length > 0))                                 
-         {                                                                                                                                                                                          
-             this.setAssigned(true);                                                                                                                                                                
-         }                                                                                                                                                                                          
-                                                                                                                                                                                                    
-     }                                                                                                                                                                                              
+  }
+}
